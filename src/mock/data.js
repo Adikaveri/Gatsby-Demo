aab2d75c8797c941422b1d79fe7795e0d3676020
Aditya Kaveri <kaveri16aditya@gmail.com>
2020-11-08 23:45:07 +0530

    message

--- src/mock/data.js
+++ src/mock/data.js
@@ -11,7 +11,7 @@ export const headData = {
 export const heroData = {
   title: '',
   name: 'Aditya Kaveri',
-  subtitle: 'Electronics and Telecommunication Engineer',
+  subtitle: 'Pursuing E&TC Engineering',
   cta: '',
 };
 
